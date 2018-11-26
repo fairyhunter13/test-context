a6ceb8862e2d6e356caeb385001c6bbd8234f2da
fairyhunter13 <fairyhunter13@gmail.com>
2018-11-26 10:57:44 +0700

    Progress: - Adding race example :construction:

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"time"
 )
@@ -12,24 +13,38 @@ const (
 
 func main() {
 	// startTime := time.Now()
-	ctx, cancel := context.WithTimeout(context.Background(), timeOut*time.Millisecond)
-	defer cancel()
-	done := make(chan int)
-	go workLong(ctx, cancel, done)
-	select {
-	case <-ctx.Done():
-		time, _ := ctx.Deadline()
-		if err := ctx.Err(); err == context.DeadlineExceeded {
-			log.Printf("Deadline already reached! Time Deadline: %v", time)
-		}
-		<-done
-		close(done)
-	case <-done:
-		log.Printf("The work is done! Deadline is not reached!")
-	}
-	time.Sleep(3000 * time.Millisecond)
-}
+	// ctx, cancel := context.WithTimeout(context.Background(), timeOut*time.Millisecond)
+	// defer cancel()
+	// done := make(chan int)
+	// go workLong(ctx, cancel, done)
+	// select {
+	// case <-ctx.Done():
+	// 	time, _ := ctx.Deadline()
+	// 	if err := ctx.Err(); err == context.DeadlineExceeded {
+	// 		log.Printf("Deadline already reached! Time Deadline: %v", time)
+	// 	}
+	// 	<-done
+	// 	close(done)
+	// case <-done:
+	// 	log.Printf("The work is done! Deadline is not reached!")
+	// }
+	// time.Sleep(3000 * time.Millisecond)
+	c := make(chan bool)
 
+	m := make(map[string]string)
+
+	go func() {
+		m["1"] = "a"
+		c <- true
+	}()
+
+	time.Sleep(1 * time.Nanosecond)
+	m["1"] = "c"
+	<-c
+	for k, v := range m {
+		fmt.Println("The key %s and value: %s", k, v)
+	}
+}
 func workLong(ctx context.Context, cancel context.CancelFunc, done chan<- int) {
 	defer func() {
 		log.Println("I have escaped with context, or maybe not!")
