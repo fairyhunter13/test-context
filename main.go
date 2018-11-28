dc75c43e78ffaa94861b64855834379a803f8a36
fairyhunter13 <fairyhunter13@gmail.com>
2018-11-28 12:26:46 +0700

    Progress: - Adding material for sharing session :construction:

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
+	"sync"
 	"time"
 )
 
@@ -12,6 +14,81 @@ const (
 )
 
 func main() {
+	// mapNotConcurrent()
+	// loopCounterCase()
+	sharedVariable()
+}
+
+func mapNotConcurrent() {
+	c := make(chan bool)
+	m := make(map[string]string)
+	go func() {
+		m["1"] = "a"
+		c <- true
+	}()
+
+	// time.Sleep(1 * time.Nanosecond)
+	m["1"] = "c"
+	<-c
+	for k, v := range m {
+		fmt.Printf("The key %s and value: %s", k, v)
+	}
+}
+
+func loopCounterCase() {
+	var wg sync.WaitGroup
+	wg.Add(5)
+	for i := 0; i < 5; i++ {
+		go func() {
+			fmt.Println(i) // Not the 'i' you are looking for.
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+}
+
+func sharedVariable() {
+	errorChannel := ParallelWrite([]byte("Test"))
+	for err := range errorChannel {
+		if err != nil {
+			fmt.Printf("There is an error! Error: %s", err)
+		}
+	}
+}
+
+// ParallelWrite writes data to file1 and file2, returns the errors.
+func ParallelWrite(data []byte) chan error {
+	res := make(chan error, 2)
+	defer close(res)
+	f1, err := os.Create("file1")
+	if err != nil {
+		res <- err
+	} else {
+		go func() {
+			// This err is shared with the main goroutine,
+			// so the write races with the write below.
+			_, err = f1.Write(data)
+			res <- err
+			f1.Close()
+		}()
+	}
+	f2, err := os.Create("file2") // The second conflicting write to err.
+	if err != nil {
+		res <- err
+	} else {
+		go func() {
+			_, err = f2.Write(data)
+			res <- err
+			f2.Close()
+		}()
+	}
+	return res
+}
+
+/* This section is only for context. */
+
+//TestContext
+func contextWorking() {
 	// startTime := time.Now()
 	// ctx, cancel := context.WithTimeout(context.Background(), timeOut*time.Millisecond)
 	// defer cancel()
@@ -29,22 +106,8 @@ func main() {
 	// 	log.Printf("The work is done! Deadline is not reached!")
 	// }
 	// time.Sleep(3000 * time.Millisecond)
-	c := make(chan bool)
-
-	m := make(map[string]string)
-
-	go func() {
-		m["1"] = "a"
-		c <- true
-	}()
-
-	time.Sleep(1 * time.Nanosecond)
-	m["1"] = "c"
-	<-c
-	for k, v := range m {
-		fmt.Println("The key %s and value: %s", k, v)
-	}
 }
+
 func workLong(ctx context.Context, cancel context.CancelFunc, done chan<- int) {
 	defer func() {
 		log.Println("I have escaped with context, or maybe not!")
@@ -54,7 +117,6 @@ func workLong(ctx context.Context, cancel context.CancelFunc, done chan<- int) {
 	log.Println("Work long succeed!")
 
 	//Maybe in here it is not executed.
-	time.Sleep(1000 * time.Millisecond)
 	log.Println("Shouldn't be happening!")
 	done <- 1
 	return
