de00cde00f57c49d326287c112dedcc7957776cb
fairyhunter13 <fairyhunter13@gmail.com>
2018-11-30 11:41:45 +0700

    Progress: - Add material for sharing session :construction:

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
@@ -13,13 +14,25 @@ const (
 )
 
 var (
-	globalCounter = 0
+	globalCounter int64
+	mutex         = new(sync.Mutex)
 )
 
 func main() {
-	// mapNotConcurrent()
-	// loopCounterCase()
-	// counterNotWrightCase()
+	problems()
+	// solution()
+}
+
+func problems() {
+	mapNotConcurrent()
+	loopCounterCase()
+	counterNotWrightCase()
+}
+
+func solution() {
+	mapNotConcurrentSolution()
+	loopCounterCaseSolution()
+	counterNotWrightCaseSolution()
 }
 
 func mapNotConcurrent() {
@@ -54,16 +67,70 @@ func loopCounterCase() {
 
 func counterNotWrightCase() {
 	fmt.Println("==========This is the loop counter section!==========")
-	done := make(chan bool)
+	var wg sync.WaitGroup
+	wg.Add(1000)
 	go func() {
 		for i := 1; i <= 1000; i++ {
 			go func() {
+				defer wg.Done()
 				globalCounter++
 			}()
 		}
-		done <- true
 	}()
-	<-done
+	wg.Wait()
+	fmt.Printf("The global counter is %d\n", globalCounter)
+
+}
+
+/* Solution for race condition problems */
+
+func mapNotConcurrentSolution() {
+	c := make(chan bool)
+	m := make(map[string]string)
+	go func() {
+		mutex.Lock()
+		m["1"] = "a"
+		mutex.Unlock()
+		c <- true
+	}()
+
+	// time.Sleep(1 * time.Nanosecond)
+	mutex.Lock()
+	m["1"] = "c"
+	mutex.Unlock()
+	<-c
+	fmt.Println("==========This is the nonconcurrent map section!==========")
+	for k, v := range m {
+		fmt.Printf("The key %s and value: %s\n", k, v)
+	}
+}
+
+func loopCounterCaseSolution() {
+	fmt.Println("==========This is the loop counter section!==========")
+	var wg sync.WaitGroup
+	wg.Add(5)
+	for i := 0; i < 5; i++ {
+		go func(i int) {
+			fmt.Println(i) // Not the 'i' you are looking for.
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+}
+
+func counterNotWrightCaseSolution() {
+	fmt.Println("==========This is the loop counter section!==========")
+	var wg sync.WaitGroup
+	wg.Add(1000)
+	go func() {
+		for i := 1; i <= 1000; i++ {
+			go func() {
+				defer wg.Done()
+				atomic.AddInt64(&globalCounter, 1)
+			}()
+		}
+	}()
+	wg.Wait()
 	fmt.Printf("The global counter is %d\n", globalCounter)
 
 }
